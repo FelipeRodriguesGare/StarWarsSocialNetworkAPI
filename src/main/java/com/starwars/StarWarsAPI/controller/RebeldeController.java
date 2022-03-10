5bc71f9e4d18212f86fe3feaccbdb95d0a09990f
ffsilva27 <filipeferreiradasilva@hotmail.com>
2022-03-09 22:09:38 -0300

    Added some Response Status.

--- src/main/java/com/starwars/StarWarsAPI/controller/RebeldeController.java
+++ src/main/java/com/starwars/StarWarsAPI/controller/RebeldeController.java
@@ -7,6 +7,7 @@ import com.starwars.StarWarsAPI.dto.RebeldeResponse;
 import com.starwars.StarWarsAPI.model.Localizacao;
 import com.starwars.StarWarsAPI.model.Rebelde;
 import com.starwars.StarWarsAPI.service.RebeldeService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -28,28 +29,46 @@ public class RebeldeController {
     @GetMapping
     @ResponseBody
     public ResponseEntity<List<RebeldeResponse>> buscarRebeldes(){
-        return ResponseEntity.ok(rebeldeResponse.toResponse(StarWarsApiApplication.bdRebeldes.listarRebeldes()));
+        List<RebeldeResponse> rebeldeResponse2 = rebeldeResponse.toResponse(StarWarsApiApplication.bdRebeldes.listarRebeldes());
+        if(rebeldeResponse2.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(rebeldeResponse2);
+        }
+        return ResponseEntity.ok().body(rebeldeResponse2);
     }
 
     @PostMapping
     public ResponseEntity<RebeldeResponse> criaRebelde(@RequestBody @Valid RebeldeRequest rebeldeRequest, UriComponentsBuilder uriComponentsBuilder){
-        Rebelde rebelde = rebeldeService.criaRebelde(rebeldeRequest);
-        URI uri = uriComponentsBuilder.path("/rebeldes/{id}").buildAndExpand(rebelde.getId()).toUri();
-        return ResponseEntity.created(uri).body(new RebeldeResponse(rebelde));
+        try{
+            Rebelde rebelde = rebeldeService.criaRebelde(rebeldeRequest);
+            URI uri = uriComponentsBuilder.path("/rebeldes/{id}").buildAndExpand(rebelde.getId()).toUri();
+            return ResponseEntity.created(uri).body(new RebeldeResponse(rebelde));
+        }catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
     }
 
     @GetMapping("/{id}")
     @ResponseBody
     public ResponseEntity<RebeldeResponse> buscaRebelde(@PathVariable UUID id) throws Exception {
-        Rebelde rebelde = StarWarsApiApplication.bdRebeldes.buscaRebelde(id);
-        return ResponseEntity.accepted().body(new RebeldeResponse(rebelde));
+        try{
+            Rebelde rebelde = StarWarsApiApplication.bdRebeldes.buscaRebelde(id);
+            return ResponseEntity.accepted().body(new RebeldeResponse(rebelde));
+        }catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
     }
 
     @PatchMapping("/atualizarlocalizacao/{id}")
     @ResponseBody
     public ResponseEntity<RebeldeResponse> atualizarLocalizacao(@PathVariable UUID id, @RequestBody LocalizacaoRequest localizacaoRequest) throws Exception {
-       Rebelde rebelde =  rebeldeService.atualizarLocalizacao(localizacaoRequest,id);
-       return ResponseEntity.ok().body(new RebeldeResponse(rebelde));
+       try{
+           Rebelde rebelde =  rebeldeService.atualizarLocalizacao(localizacaoRequest,id);
+           return ResponseEntity.ok().body(new RebeldeResponse(rebelde));
+       }catch (Exception e){
+           return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+       }
+
     }
 
     @PatchMapping("/traidor/{id}")
--- src/main/java/com/starwars/StarWarsAPI/dto/LocalizacaoRequest.java
+++ src/main/java/com/starwars/StarWarsAPI/dto/LocalizacaoRequest.java
@@ -2,7 +2,6 @@ package com.starwars.StarWarsAPI.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-
 import java.util.UUID;
 
 @Getter @Setter
